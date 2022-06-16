3a564527a74270288daec6c4f479790372a353c2
joyunju <whdbswn1113@naver.com>
2022-06-16 16:31:18 +0900

    service

--- src/main/java/com/javaex/service/PhoneService.java
+++ src/main/java/com/javaex/service/PhoneService.java
@@ -10,32 +10,57 @@ import com.javaex.vo.PersonVo;
 
 @Service
 public class PhoneService {
-	
+
+	// 필드
 	@Autowired
 	private PhoneDao phoneDao;
-	
-	public List<PersonVo> getPersonList(){
-		return phoneDao.getPersonList();
-	}
-	
-	public int personInsert(PersonVo personVo) {
-		return phoneDao.personInsert(personVo);
+	// 생성자
+
+	// 메소드-gs
+
+	// 메소드-일반
+
+	// 전화번호 리스트
+	public List<PersonVo> getPersonList() {
+		System.out.println("PhoneService>getPersonList()");
+
+		List<PersonVo> personList = phoneDao.getPersonList();
+
+		return personList;
 	}
 
-	public int personDelete(int personId) {
-		return phoneDao.personDelete(personId);
+	// 전화번호 등록
+	public int personInsert(PersonVo vo) {
+		System.out.println("PhoneService>PersonInsert()");
+
+		int count = phoneDao.personInsert(vo);
+
+		return count;
 	}
-	/*
-	public int personUpdate(PersonVo personVo) {
-		return phoneDao.personUpdate(personVo);
+
+	// 전화번호 삭제
+	public int personDelete(int no) {
+		System.out.println("PhoneService>personDelete()");
+		int count = phoneDao.personDelete(no);
+
+		return count;
 	}
-	
-	public PersonVo getPerson(int personId) {
-		return phoneDao.getPerson(personId);
+
+	// 전화번호 수정폼(정보가져오기)
+	public PersonVo getPerson(int no) {
+		System.out.println("PhoneService>getPerson()");
+		PersonVo personVo = phoneDao.getPerson(no);
+
+		return personVo;
+
 	}
-	
-	*/
-	
-	
-	
+
+	// 전화번호 수정
+	public int personUpdate(PersonVo vo) {
+		System.out.println("PhoneService>personUpdate()");
+		int count = phoneDao.personUpdate(vo);
+
+		return count;
+	}
+
 }
